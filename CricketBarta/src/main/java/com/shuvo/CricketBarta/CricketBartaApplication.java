174edf1a82d451fdd370c12ddaa0e4d7f6e10a50
mjoyshuvo <mjoyshuvo@gmail.com>
2016-03-18 00:49:36 +0600

    added for the first time

---
+++ CricketBarta/src/main/java/com/shuvo/CricketBarta/CricketBartaApplication.java
@@ -0,0 +1,12 @@
+package com.shuvo.CricketBarta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketBartaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketBartaApplication.class, args);
+	}
+}
---
+++ CricketBarta/src/test/java/com/shuvo/CricketBarta/CricketBartaApplicationTests.java
@@ -0,0 +1,18 @@
+package com.shuvo.CricketBarta;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CricketBartaApplication.class)
+@WebAppConfiguration
+public class CricketBartaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
